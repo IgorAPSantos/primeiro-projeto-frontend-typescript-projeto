9f9076a8a79e9fb35ac97b9c227e0b90abc2f961
IgorAPSantos <igorppandre@hotmail.com>
2023-12-15 23:49:48 -0300

    created teatcher card

--- base/src/pages/Main/index.tsx
+++ base/src/pages/Main/index.tsx
@@ -1,10 +1,15 @@
 import './styles.css';
 import Header from '../../components/Header';
+import TeacherCard from '../../components/TeacherCard';
 
 function Main() {
   return (
     <div className='container'>
-      <h1><Header /></h1>
+      <Header />
+
+      <div>
+        <TeacherCard teacher={{ id: 1, name: 'Daniel', avatar: 'https://media.licdn.com/dms/image/D4D03AQEBbT7FDX7ZJw/profile-displayphoto-shrink_800_800/0/1692652307494?e=1707955200&v=beta&t=FElrqRdi5RvZhQ_yB3A3HJHXt_u0SugZefTPwFrPyRI' }} />
+      </div>
     </div>
   );
 }
